3332a446e2ab6e469e4ccff331c87f5101f0544a
chenqiwei <chqiwei@gmail.com>
2014-09-18 23:30:02 +0800

    [add:kernel] learn create pc linux kernel module

---
+++ pc_kernel_module/mdl.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+static int __init mdl_init(void)
+{
+	printk(KERN_INFO"hello pc module\n");
+	return 0;
+}
+
+static void __exit mdl_exit(void)
+{
+	printk(KERN_INFO"bye kernel.\n");
+}
+
+module_init(mdl_init);
+module_exit(mdl_exit);
+
+
