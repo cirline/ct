58b43fd20f7a3f33e2412513efcc7096da5114f5
chenqiwei <chqiwei@gmail.com>
2015-02-25 23:05:48 +0800

    [opt:timer] compatible timer0 and timer1

--- timer.c
+++ timer.c
@@ -1,28 +1,34 @@
 #include "common.h"
 #include "s5p_regs.h"
+#include "timer.h"
 
-void inline timer_update(void)
+void inline timer_update(enum timer_sn t_sn)
 {
-	region_write(TCON, 0x1, 9, 1);
+	region_write(TCON, 0x1, (t_sn == TIMER0) ? 1 : ((t_sn<<2) + 5), 1);
 }
 
-void timer_toggle(int x)
+void timer_toggle(enum timer_sn tn, int x)
 {
-	region_write(TCON, 0x1, 9, 0);
-	region_write(TCON, 0x1, 8, (x & 1));
+	region_write(TCON, 0x1, (t_sn ==  TIMER0) ? 1 : ((t_sn<<2) + 5), 0);
+	region_write(TCON, 0x1, (t_sn ==  TIMER0) ? 0 : ((t_sn<<2) + 4), (x & 1));
 }
 
-int timer_init(void)
+int timer_init(enum timer_sn t_sn, int cnt, int lev)
 {
-	region_write(GPDxCON(0), 0xf, 4, 0x2);
-
-	region_write(TCFG0, 0xff, 0, 0xff);
-	region_write(TCFG1, 0xf, 4, 0x4);
-//	region_write(TCON, 0xf, 8, ((1<<3)|(1<<2)));
-	region_write(TCON, 0xf, 8, (1<<2));
-	__raw_write(TCNTBx(1), 16110);
-	__raw_write(TCMPBx(1), 8550);
-	timer_update();
+	/* set tout */
+	region_write(GPDxCON(0), 0xf, (t_sn<<2), 0x2);
+	/* set prescaler */
+	region_write(TCFG0, 0xffff, 0, 0xffff);
+	/* set divider */
+	region_write(TCFG1, 0xf, (t_sn<<2), 0x4);
+	/* set timer */
+	region_write(TCON, 0xf, (t_sn == TIMER0) ? 0 : ((t_sn<<2) + 4), (1<<3)|(1<<2));
+	/* set period */
+//	__raw_write(TCNTBx(tn), 16110);
+//	__raw_write(TCMPBx(tn), 8550);
+	__raw_write(TCNTBx(t_sn), cnt);
+	__raw_write(TCMPBx(t_sn), lev);
+	timer_update(t_sn);
 
 	return 0;
 }
--- timer.h
+++ timer.h
@@ -1,9 +1,12 @@
 #ifndef __TIMER_H__
 #define __TIMER_H__
 
-extern void timer_update(void);
-extern void timer_toggle(int x);
-extern int timer_init(void);
+/* timer serial number */
+enum timer_sn { TIMER0 = 0, TIMER1, TIMER2, TIMER3 };
+
+extern void inline timer_update(enum timer_sn t_sn);
+extern void timer_toggle(enum timer_sn t_sn, int x);
+extern int timer_init(enum timer_sn t_sn, int cnt, int lev);
 
 #endif
 
