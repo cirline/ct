96afa4dedd89953d4a2f6b923a76ee4af13dea2b
chenqiwei <chqiwei@gmail.com>
2015-07-18 02:25:44 +0800

    ldd: two thread module

--- ldd/sem.c
+++ ldd/sem.c
@@ -2,10 +2,12 @@
 #include <linux/module.h>
 #include <linux/workqueue.h>
 #include <linux/semaphore.h>
+#include <linux/delay.h>
 
-static struct work_struct module_work1;
+static struct work_struct module_work1, module_work2;
 static struct workqueue_struct * module_works_queue;
-static struct semaphore sem1;
+static struct workqueue_struct * module_works_queue2;
+static struct semaphore sem1, sem2;
 
 static void module_work1_func(struct work_struct *work)
 {
@@ -16,7 +18,21 @@ static void module_work1_func(struct work_struct *work)
 			printk("down sem failed.\n");
 			return;
 		}
-		printk("module loop %d\n", i);
+		printk("work1 module loop %d\n", i);
+		up(&sem2);
+	}
+}
+
+static void module_work2_func(struct work_struct *work)
+{
+	int i;
+    printk("hello ws2. \n");
+	for(i = 0; i < 8; i++) {
+		if(down_interruptible(&sem2)) {
+			printk("down sem failed.\n");
+			return;
+		}
+		printk("work2 module loop %d\n", i);
 		up(&sem1);
 	}
 }
@@ -26,6 +42,7 @@ static int __init module_init_func(void)
     printk("hello module\n");
 
 	sema_init(&sem1, 1);
+	sema_init(&sem2, 0);
 
     module_works_queue = create_singlethread_workqueue("module_works_queue");
     if(!module_works_queue) {
@@ -33,17 +50,31 @@ static int __init module_init_func(void)
         return -1;
     }
 
-    INIT_WORK(&module_work1, module_work1_func);
+    module_works_queue2 = create_singlethread_workqueue("module_works_queue2");
+    if(!module_works_queue2) {
+        printk("create_singlethread_workqueue2 failed! \n");
+		goto create_thread_queue2_failed;
+    }
 
+    INIT_WORK(&module_work1, module_work1_func);
+    INIT_WORK(&module_work2, module_work2_func);
+
+	printk("start work1.\n");
     queue_work(module_works_queue, &module_work1);
+	printk("start work2.\n");
+    queue_work(module_works_queue2, &module_work2);
 
     return 0;
+create_thread_queue2_failed:
+	destroy_workqueue(module_works_queue);
+	return -1;
 }
 
 static void __exit module_exit_func(void)
 {
     printk("bye, module\n");
-    destroy_workqueue(module_works_queue);
+    destroy_workqueue(module_works_queue1);
+    destroy_workqueue(module_works_queue2);
 }
 
 module_init(module_init_func);
