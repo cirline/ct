8eb2b58212758df55dd50d6cb8078f2cefd470ec
chenqiwei <chqiwei@gmail.com>
2015-07-18 01:00:23 +0800

    ldd: one sem test

--- ldd/sem.c
+++ ldd/sem.c
@@ -1,18 +1,31 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
+#include <linux/semaphore.h>
 
 static struct work_struct module_work1;
 static struct workqueue_struct * module_works_queue;
+static struct semaphore sem1;
 
 static void module_work1_func(struct work_struct *work)
 {
+	int i;
     printk("hello ws1. \n");
+	for(i = 0; i < 8; i++) {
+		if(down_interruptible(&sem1)) {
+			printk("down sem failed.\n");
+			return;
+		}
+		printk("module loop %d\n", i);
+		up(&sem1);
+	}
 }
 
 static int __init module_init_func(void)
 {
     printk("hello module\n");
+
+	sema_init(&sem1, 1);
 
     module_works_queue = create_singlethread_workqueue("module_works_queue");
     if(!module_works_queue) {
