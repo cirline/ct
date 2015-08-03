5680a527ff15b5ef549e164a9ca101cad35b6507
chqw <chenqiwei@szhklt.com>
2015-08-03 20:12:20 +0800

    ldd: sem

--- ldd/sem.c
+++ ldd/sem.c
@@ -1,18 +1,52 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
+#include <linux/semaphore.h>
 
-static struct work_struct module_work1;
 static struct workqueue_struct * module_works_queue;
+static struct work_struct module_work1, module_work2;
+static struct semaphore sem1, sem2;
 
 static void module_work1_func(struct work_struct *work)
 {
-    printk("hello ws1. \n");
+    int i;
+
+    for(i = 0; i < 8; i++) {
+        if(down_interruptible(&sem1)) {
+            printk("work1 require sem1 failed.\n");
+            continue;
+        }
+        printk("work1, %d\n", i);
+        up(&sem2);
+    }
+}
+
+static void module_work2_func(struct work_struct *work)
+{
+    int i;
+
+    for(i = 0; i < 8; i++) {
+        if(down_interruptible(&sem2)) {
+            printk("work2 require sem2 failed.\n");
+            continue;
+        }
+        printk("work2, %d\n", i);
+        up(&sem1);
+    }
 }
 
 static int __init module_init_func(void)
 {
     printk("hello module\n");
+
+    sema_init(&sem1, 1);
+    sema_init(&sem2, 1);
+    /*
+    if(down_interruptible(&sem2)) {
+        printk("down the sem2 failed.\n");
+        goto out;
+    }
+    */
 
     module_works_queue = create_singlethread_workqueue("module_works_queue");
     if(!module_works_queue) {
@@ -21,10 +55,14 @@ static int __init module_init_func(void)
     }
 
     INIT_WORK(&module_work1, module_work1_func);
+    INIT_WORK(&module_work2, module_work2_func);
 
     queue_work(module_works_queue, &module_work1);
+    queue_work(module_works_queue, &module_work2);
 
     return 0;
+out:
+    return -1;
 }
 
 static void __exit module_exit_func(void)
---
+++ ldd/simple_module.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static __init int sm_init(void)
+{
+	printk("hello init\n");
+	return 0;
+}
+
+static __exit void sm_exit(void)
+{
+	printk("bye, exit\n");
+}
+
+module_init(sm_init);
+module_exit(sm_exit);
+
+
+
+
+
