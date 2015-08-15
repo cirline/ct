e0eb5017b16f7fdc2cc2a01aefdcd91b33d994a6
chqw <chenqiwei@szhklt.com>
2015-08-15 10:40:46 +0800

    ldd: device model driver

--- ldd/devmod/devmod.c
+++ ldd/devmod/devmod.c
@@ -27,7 +27,6 @@ struct bus_attribute kernsm_bus_attr = {
     .show = kernsm_bus_show,
     .store = kernsm_bus_store,
 };
-
 //BUS_ATTR();
 
 struct bus_type kernsm_bus_type = {
@@ -35,6 +34,27 @@ struct bus_type kernsm_bus_type = {
     .match = kernsm_match,
     //.hotplug = kernsm_hotplug,
 };
+
+struct _kernsm_driver {
+    char *version;
+    struct module *module;
+    struct device_driver driver;
+    struct driver_attribute drv_attr;
+};
+
+struct _kernsm_driver kernsm_driver = {
+    .version = "kernsm driver v1",
+    .module = THIS_MODULE,
+    .driver = {
+        .name = "kernsm_driver",
+    },
+};
+
+ssize_t kernsm_driver_attr_show(struct device_driver *driver, char *buf)
+{
+    struct _kernsm_driver *drv = container_of(driver, struct _kernsm_driver, driver);
+    return snprintf(buf, PAGE_SIZE, "%s\n", drv->version);
+}
 
 static __init int sm_init(void)
 {
@@ -53,9 +73,29 @@ static __init int sm_init(void)
         goto create_attr_failed;
     }
 
+    /* driver */
+    kernsm_driver.driver.bus = &kernsm_bus_type;
+    ret = driver_register(&kernsm_driver.driver);
+    if(ret) {
+        printk("kernsm driver register failed !!! \n");
+        goto register_driver_failed;
+    }
+    kernsm_driver.drv_attr.attr.name = "version";
+    kernsm_driver.drv_attr.attr.mode = S_IRUGO | S_IWUSR;
+    kernsm_driver.drv_attr.show = kernsm_driver_attr_show;
+    kernsm_driver.drv_attr.store = NULL;
+    ret = driver_create_file(&kernsm_driver.driver, &kernsm_driver.drv_attr);
+    if(ret) {
+        printk("kernsm driver create file failed !!! \n");
+        goto driver_create_file_failed;
+    }
 
 	return 0;
 
+driver_create_file_failed:
+    driver_unregister(&kernsm_driver.driver);
+register_driver_failed:
+    bus_remove_file(&kernsm_bus_type, &kernsm_bus_attr);
 create_attr_failed:
     bus_unregister(&kernsm_bus_type);
     return -1;
@@ -64,6 +104,8 @@ create_attr_failed:
 static __exit void sm_exit(void)
 {
 	printk("bye, exit\n");
+    driver_remove_file(&kernsm_driver.driver, &kernsm_driver.drv_attr);
+    driver_unregister(&kernsm_driver.driver);
     bus_remove_file(&kernsm_bus_type, &kernsm_bus_attr);
     bus_unregister(&kernsm_bus_type);
 }
