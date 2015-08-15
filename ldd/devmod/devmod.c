70d83cfbb7c06159309c8292a53fdd938ee0d4ac
chqw <chenqiwei@szhklt.com>
2015-08-15 14:20:27 +0800

    ldd: device model device

--- ldd/devmod/devmod.c
+++ ldd/devmod/devmod.c
@@ -35,6 +35,16 @@ struct bus_type kernsm_bus_type = {
     //.hotplug = kernsm_hotplug,
 };
 
+void kernsm_bus_release(struct device *dev)
+{
+    printk("kernsm bus release. \n");
+}
+
+struct device kernsm_bus = {
+    .init_name = "kernsm_bus0",
+    .release = kernsm_bus_release,
+};
+
 struct _kernsm_driver {
     char *version;
     struct module *module;
@@ -56,6 +66,30 @@ ssize_t kernsm_driver_attr_show(struct device_driver *driver, char *buf)
     return snprintf(buf, PAGE_SIZE, "%s\n", drv->version);
 }
 
+/* device */
+struct _kernsm_device {
+    char *version;
+    char *name;
+    struct _kernsm_driver *driver;
+    struct device device;
+    struct device_attribute dev_attr;
+};
+
+void kernsm_device_release(struct device *dev)
+{
+    printk("kernsm release device ..\n");
+}
+
+struct _kernsm_device kernsm_device = {
+    .version = "kernsm device v1",
+    .name = "kernsm_driver",
+    .device = {
+        .bus = &kernsm_bus_type,
+        .parent = &kernsm_bus,
+        .release = kernsm_device_release,
+    },
+};
+
 static __init int sm_init(void)
 {
     int ret;
@@ -73,6 +107,13 @@ static __init int sm_init(void)
         goto create_attr_failed;
     }
 
+    /* bus device */
+    ret = device_register(&kernsm_bus);
+    if(ret) {
+        printk("register bus device failed !!!\n");
+        goto register_bus_device_failed;
+    }
+
     /* driver */
     kernsm_driver.driver.bus = &kernsm_bus_type;
     ret = driver_register(&kernsm_driver.driver);
@@ -81,6 +122,7 @@ static __init int sm_init(void)
         goto register_driver_failed;
     }
     kernsm_driver.drv_attr.attr.name = "version";
+//    kernsm_driver.drv_attr.attr.owner = kernsm_driver.module;
     kernsm_driver.drv_attr.attr.mode = S_IRUGO | S_IWUSR;
     kernsm_driver.drv_attr.show = kernsm_driver_attr_show;
     kernsm_driver.drv_attr.store = NULL;
@@ -90,22 +132,36 @@ static __init int sm_init(void)
         goto driver_create_file_failed;
     }
 
-	return 0;
+    /* device */
+    kernsm_device.device.init_name = kernsm_device.name;
+    ret = device_register(&kernsm_device.device);
+    if(ret) {
+        printk("register kernsm device failed !!! \n");
+        goto register_device_failed;
+    }
 
+	return ret;
+
+register_device_failed:
+    driver_remove_file(&kernsm_driver.driver, &kernsm_driver.drv_attr);
 driver_create_file_failed:
     driver_unregister(&kernsm_driver.driver);
 register_driver_failed:
+    device_unregister(&kernsm_bus);
+register_bus_device_failed:
     bus_remove_file(&kernsm_bus_type, &kernsm_bus_attr);
 create_attr_failed:
     bus_unregister(&kernsm_bus_type);
-    return -1;
+    return ret;
 }
 
 static __exit void sm_exit(void)
 {
 	printk("bye, exit\n");
+    device_unregister(&kernsm_device.device);
     driver_remove_file(&kernsm_driver.driver, &kernsm_driver.drv_attr);
     driver_unregister(&kernsm_driver.driver);
+    device_unregister(&kernsm_bus);
     bus_remove_file(&kernsm_bus_type, &kernsm_bus_attr);
     bus_unregister(&kernsm_bus_type);
 }
