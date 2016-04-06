82b66626507c4d64f822cdf0abb565fc99b969e9
chenqiwei <chenqiwei@szhklt.com>
2016-04-06 16:05:17 +0800

    android: tools

---
+++ android/tools/src/com/chenqiwei/tools/MainActivity.java
@@ -0,0 +1,76 @@
+package com.chenqiwei.tools;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+	public static final String tag = "chenqiwei.tools";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+    	private TextView tv_phone_ip;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            
+            tv_phone_ip = (TextView) rootView.findViewById(R.id.tv_phone_ip);
+            
+            Status status = new Status(this.getActivity());
+            Log.i(tag, "ip = " + status.getIP());
+            tv_phone_ip.setText(status.getIP());
+            
+            return rootView;
+        }
+    }
+
+}
