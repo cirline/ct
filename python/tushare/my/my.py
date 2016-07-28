805bf86bcb015d276cb866e6a1982ae9c2a26c0b
chenqiwei.e3 <chqiwei89@163.com>
2016-07-29 01:40:56 +0800

    python: tushare add all stocks

--- python/tushare/my/my.py
+++ python/tushare/my/my.py
@@ -31,9 +31,14 @@ def update_record(cursor, table, code, column, value):
         sql = "insert into %s (code, %s) values ('%s', '%s');" % (table, column, code, value)
     cursor.execute(sql)
 
+def get_last_update(cursor, code):
+    sql = "select * from stocks where code = '%s'" % code
+
 def get_lowest(code):
     now = datetime.now()
     date_now = now.strftime('%Y-%m-%d')
+
+    date_last_update = get_last_update(code)
 
     one_month_ago = now - timedelta(days = 30)
     date_1m_ago = one_month_ago.strftime('%Y-%m-%d')
@@ -41,26 +46,38 @@ def get_lowest(code):
     data = ts.get_hist_data(code, date_1m_ago, date_now)
     close = data['close']
 
-    print(close)
+    lowest = 9999;
+    try:
+        print("data number: %d" % len(close))
+        lowest = close[0]
+        for p in close:
+            if lowest > p:
+                lowest = p
+    except Exception as e:
+        print(e)
 
-    lowest = close[0]
-    for p in close:
-        if lowest > p:
-            lowest = p
     return lowest
 
 def update_stock_lowest(cursor, code):
     lowest = get_lowest(code)
     update_record(cursor, "stocks", code, "lowest", lowest)
+    return lowest
 
 conn = sqlite3.connect('my.db')
 cursor = conn.cursor()
 if not check_column(cursor, "stocks", "lowest"):
     insert_column(cursor, "stocks", "lowest", "varchar", 16)
+if not check_column(cursor, "stocks", "last_update"):
+    insert_column(cursor, "stocks", "last_update", "varchar", 32)
+"""
+allstocks = ts.get_stock_basics()
 
-update_stock_lowest(cursor, "601398")
-
-
+i = 0
+for stock in allstocks.index:
+    lowest = update_stock_lowest(cursor, stock)
+    print("%d: %s - %f" % (i, stock, lowest))
+    conn.commit()
+    i = i + 1
+"""
 cursor.close()
-conn.commit()
 conn.close()
