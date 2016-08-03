edbd3e547ce98f2485b009573f0a455055689975
chenqiwei <chenqiwei@szhklt.com>
2016-08-03 18:01:26 +0800

    python: tushare update

--- python/tushare/my/my.py
+++ python/tushare/my/my.py
@@ -5,63 +5,99 @@ import tushare as ts
 import sqlite3
 from mydb import *
 
-def update_last_update(db, code):
+"""
+* record_up_to_date - check the record is up-to-date
+*
+* the return value is an unicode of the last_update or None,
+* or 'up-to-date' while record up-to-date
+"""
+def record_last_update(db, code):
+    cell = 'last_update'
+
     now = datetime.now()
     date_now = now.strftime('%Y-%m-%d')
-    db.update_onecell('stocks', 'code', code, 'last_update', date_now)
-
-def get_lowest(db, code, item):
-    now = datetime.now()
-    date_now = now.strftime('%Y-%m-%d')
-    lowest = 9999
-
-    date_last_update = db.get_onecell('stocks', 'code', code, 'last_update')
+    date_last_update = db.get_onecell('stocks', 'code', code, cell)
     if(date_last_update == None):
-        one_month_ago = now - timedelta(days = 5)
-        date_start = one_month_ago.strftime('%Y-%m-%d')
+        return None
+    elif(date_last_update == now):
+        return 'up-to-date'
     else:
-        date_start = date_last_update
-        lowest = db.get_onecell('stocks', 'code', code, item + '_lowest')
-        if(lowest == None):
-            lowest = 9999
+        return date_last_update
 
-    # print("now: %s, start: %s, lowest = %s" % (date_now, date_start, lowest))
-    if(date_now == date_start):
-        return 0
+def record_update_last_update(db, code):
+    cell = 'last_update'
+
+    now = datetime.now()
+    date_now = now.strftime('%Y-%m-%d')
+    db.update_onecell('stocks', 'code', code, cell, date_now)
+
+def get_now_date():
+    now = datetime.now()
+    return now.strftime('%Y-%m-%d')
+
+def pull_net_data(code, last_update, default_day):
+    now = datetime.now()
+    date_now = now.strftime('%Y-%m-%d')
+
+    if(last_update == None):
+        temp = now - timedelta(days = default_day)
+        last_update = temp.strftime('%Y-%m-%d')
+
+    print('%s ~ %s' % (last_update, date_now))
+    return ts.get_hist_data(code, last_update, date_now)
+
+def get_lowest(db, data, code, item, last_update, defval):
+    if(last_update != None):
+        lowest = db.get_onecell('stocks', 'code', code, item + '_lowest')
+        if(lowest != None):
+            defval = lowest
 
     try:
-        data = ts.get_hist_data(code, date_start, date_now)
         item_data = data[item]
+        print("item data len: %d" % (len(item_data)))
         for p in item_data:
-            if lowest > p:
-                lowest = p
+            if(isinstance(p, float) and defval > p):
+                defval = p
     except Exception as e:
         print(e)
 
-    return lowest
+    return defval
 
-def update_lowest(db, code, item):
-    lowest = get_lowest(db, code, item)
+def record_update_lowest(db, data, code, item, last_update):
+    lowest = get_lowest(db, data, code, item, last_update, 9999)
     if(lowest > 0 and lowest < 9999):
         db.update_onecell('stocks', 'code', code, item + '_lowest', lowest)
     return lowest
 
+def record_update_newest(db, code, item):
+    pass
+
+def update_record(db, code):
+    last_update = record_last_update(db, code)
+    if(last_update != 'up-to-date'):
+        print("%s pulling ..." % code)
+        data = pull_net_data(code, last_update, 7)
+        print('%s update ma5 lowest ...' % code)
+        record_update_lowest(db, data, code, 'ma5', last_update)
+        record_update_last_update(db, code)
+    else:
+        print("%s up-to-date" % code)
+
 db = mydb('my.db')
 dbcolumns = []
-dbcolumns.append(['code', 32])
-dbcolumns.append(['ma5_lowest', 32])
-dbcolumns.append(['last_update', 32])
+dbcolumns.append(['code', 'varchar(32)'])
+dbcolumns.append(['ma5_lowest', 'float'])
+dbcolumns.append(['close', 'float'])
+dbcolumns.append(['last_update', 'varchar(32)'])
 db.check_db('stocks', dbcolumns)
 
 allstocks = ts.get_stock_basics()
 
-i = 0
 for code in allstocks.index:
-    lowest = update_lowest(db, code, 'ma5')
-    update_last_update(db, code)
-    print("%d: %s - %f\n" % (i, code, lowest))
+    print("%s:" % code)
+    update_record(db, code)
     db.commit()
-    i = i + 1
+    print("\n")
 
 db.close()
 
