e2f53a1ee441a6466225f0c6efa6b0da7c7dc74b
chenqiwei <chenqiwei@szhklt.com>
2016-08-24 10:59:27 +0800

    lab: update tushare

--- lab/tushare/main.py
+++ lab/tushare/main.py
@@ -5,26 +5,27 @@ import threading
 import time as ostm
 import tushare as ts
 from datetime import *
+import string
 
 class main_window(wx.Frame):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title = title, size = (80, 150))
+        wx.Frame.__init__(self, parent, title = title, size = (80, 100))
 
         self.initUI()
 
     def initUI(self):
+        self.statusBar = self.CreateStatusBar()
+
         self.mainPanel = wx.Panel(self)
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         self.mainPanel.SetSizer(self.mainSizer)
 
-        sbs = self.initStaticBoxSizer(self.mainPanel, "", wx.HORIZONTAL, self.mainSizer)
-        self.stime = self.initStaticText(self.mainPanel, 'time', sbs)
+        sbs = self.initStaticBoxSizer(self.mainPanel, "", wx.VERTICAL, self.mainSizer)
 
         self.sPrice = []
         for ss in slist:
             print ss
-            sbs = self.initStaticBoxSizer(self.mainPanel, ss, wx.HORIZONTAL, self.mainSizer)
-            st = self.initStaticText(self.mainPanel, 'static text', sbs)
+            st = self.initStaticText(self.mainPanel, '0', sbs)
             self.sPrice.append(st)
 
     def initStaticBoxSizer(self, parent, label, ori, layout):
@@ -38,12 +39,22 @@ class main_window(wx.Frame):
         layout.Add(st)
         return st
 
-    def updateUI(self, time, msg):
-        self.stime.SetLabel(time)
-        i = 0
-        while(i < len(slist)):
-            self.sPrice[i].SetLabel(msg[i])
-            i = i + 1
+    def updateUI(self, time, msg, update):
+        self.statusBar.SetStatusText(time)
+        if(update):
+            i = 0
+            while(i < len(slist)):
+                pp = self.sPrice[i].GetLabel()
+                self.sPrice[i].SetLabel(msg[i])
+                ppf = string.atof(pp)
+                nnf = string.atof(msg[i])
+                if(nnf > ppf):
+                    self.sPrice[i].SetForegroundColour('red')
+                elif(nnf < ppf):
+                    self.sPrice[i].SetForegroundColour('blue')
+                else:
+                    self.sPrice[i].SetForegroundColour('blank')
+                i = i + 1
 
 class TushareThread(threading.Thread):
     def __init__(self, context):
@@ -52,14 +63,24 @@ class TushareThread(threading.Thread):
     
     def run(self):
         print('running ...')
+        i = 0
+        update = False
+        price = []
         while(True):
             now = datetime.now().strftime('%H:%M:%S')
-            price = []
-            for ss in slist:
-                df = ts.get_realtime_quotes(ss)
-                price.append(df['price'][0])
-            wx.CallAfter(self.context.updateUI, now, price)
-            ostm.sleep(3)
+            if(i == 0):
+                price = []
+                for ss in slist:
+                    df = ts.get_realtime_quotes(ss)
+                    price.append(df['price'][0])
+                update = True
+            wx.CallAfter(self.context.updateUI, now, price, update)
+            ostm.sleep(1)
+            if(i < 4):
+                i = i + 1
+            else:
+                i = 0
+            update = False
 
 slist = []
 slist.append('601398')
