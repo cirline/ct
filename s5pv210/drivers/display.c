4e726f701044bd5f396a479994c80f9f48b09d71
chenqiwei.e3 <chqiwei89@163.com>
2016-09-12 00:42:22 +0800

    s5pv210, lcd hv signal

--- s5pv210/drivers/display.c
+++ s5pv210/drivers/display.c
@@ -1,10 +1,10 @@
 #include <display.h>
 #include <common.h>
 
-#define SIG_LOW		"|    "" "
-#define SIG_HIGH	"    |"" "
-#define SIG_BOTH	"  x  "" "
-#define SIG_NONE	"  -  "" "
+#define SIG_LOW		"|    ""  "
+#define SIG_HIGH	"|||||""  "
+#define SIG_BOTH	"  x  ""  "
+#define SIG_NONE	"  -  ""  "
 
 
 void __print_hv_signal(int sig)
@@ -28,6 +28,7 @@ void print_hv_signal(struct hv_signal *s)
 {
 	__print_hv_signal(s->vs);
 	__print_hv_signal(s->hs);
+	__print_hv_signal(s->de);
 	__print_hv_signal(s->dclk);
 	printf("\n");
 }
@@ -55,10 +56,28 @@ int generate_hv_signal(struct hv_config *hvc)
 	while(1) {
 		int vspw = hvc->vspw;
 		int frameline = hvc->frameline;
+		int vbpd = hvc->vbpd;
+		int vfpd = hvc->vfpd;
+		int lineval = hvc->lineval;
+		int vflag = 0;
 		for(; frameline > 0; frameline--) {
 
 			int linehoz = hvc->linehoz;
 			int hspw = hvc->hspw;
+			int hbpd = hvc->hbpd;
+			int hfpd = hvc->hfpd;
+			int hozval = hvc->hozval;
+
+			if(vbpd > 0) {
+				vflag = 0;
+				vbpd--;
+			} else if(lineval > 0) {
+				vflag = 1;
+				lineval--;
+			} else if(vfpd > 0) {
+				vflag = 0;
+				vfpd--;
+			}
 			for(; linehoz > 0; linehoz--) {
 				// vs signal
 				if(vspw > 0) {
@@ -73,6 +92,22 @@ int generate_hv_signal(struct hv_config *hvc)
 					hspw--;
 				} else
 					sig.hs = HVS_HIGH;
+
+
+				// de signal
+				if(vflag) {
+					if(hbpd > 0) {
+						sig.de = HVS_LOW;
+						hbpd--;
+					} else if(hozval > 0) {
+						sig.de = HVS_HIGH;
+						hozval--;
+					} else if(hfpd > 0) {
+						sig.de = HVS_LOW;
+						hfpd--;
+					}
+				} else
+					sig.de = HVS_LOW;
 
 				generate_hv_clk(&sig);
 			}
