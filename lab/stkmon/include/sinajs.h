1e280372a571ba9d583cfb5d1311ab5f9ba7f2d4
chenqiwei <chenqiwei@szhklt.com>
2016-09-14 16:54:39 +0800

    stkmon: autotool gtk

--- lab/stkmon/include/sinajs.h
+++ lab/stkmon/include/sinajs.h
@@ -1,32 +1,18 @@
 #ifndef __SINAJS_H__
 #define __SINAJS_H__
 
-#define HTTP_FORMAT_STRING	"var hq_str_%s=\"%s,%f,%f,%f,%f,%f,%f,%f,%ld,%ld,%d,%f,%d,%f,%d,%f,%d,%f,%d,%f,%d,%f,%d,%f,%d,%f,%d,%f,%d,%f,%s,%s,%s\";"
-
-#define HTTP_ARGS_LIST(p)	\
-	(p)->code,(p)->name,&(p)->open,&(p)->pre_close,&(p)->price,&(p)->high,&(p)->low,\
-	&(p)->bid,&(p)->ask,&(p)->volume,&(p)->amount,\
-	&(p)->b1_v,&(p)->b1_p,&(p)->b2_v,&(p)->b2_p,&(p)->b3_v,&(p)->b3_p,&(p)->b4_v,&(p)->b4_p,&(p)->b5_v,&(p)->b5_p,\
-	&(p)->a1_v,&(p)->a1_p,\
-	&(p)->a2_v,&(p)->a2_p,\
-	&(p)->a3_v,&(p)->a3_p,\
-	&(p)->a4_v,&(p)->a4_p,\
-	&(p)->a5_v,&(p)->a5_p,\
-	(p)->date,(p)->time,(p)->reserve
-
-#define QTEST	"ab\"cd\"ef"
-
 struct sinajs {
 	char code[16];
 	char name[20];
 	float open, pre_close;
 	float price, high, low;
 	float bid, ask;
-	long volume, amount;
-	int b1_v, b2_v, b3_v, b4_v, b5_v;
-	float b1_p, b2_p, b3_p, b4_p, b5_p;
-	int a1_v, a2_v, a3_v, a4_v, a5_v;
-	float a1_p, a2_p, a3_p, a4_p, a5_p;
+	long volume;
+	long amount;
+	int bv[5];
+	float bp[5];
+	int av[5];
+	float ap[5];
 	char date[16];
 	char time[16];
 	char reserve[16];
--- lab/stkmon/src/main.c
+++ lab/stkmon/src/main.c
@@ -1,25 +1,130 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include <gtk/gtk.h>
+
 #include <ccutils/net.h>
 #include <ccutils/log.h>
 
 #include "sinajs.h"
+
+int main_ui(int argc, char *argv[])
+{
+	GtkWidget *win;
+	gtk_init(&argc, &argv);
+
+	win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(win), "center");
+	gtk_window_set_default_size(GTK_WINDOW(win), 250, 100);
+	gtk_widget_show_all(win);
+	g_signal_connect(win, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+	gtk_main();
+
+	return 0;
+}
+
+void psinajs(struct sinajs *sj)
+{
+	int i;
+
+	printf("code: %s\n", sj->code);
+	printf("open: %f\n", sj->open);
+	printf("pre_close: %f\n", sj->pre_close);
+	printf("price: %f\n", sj->price);
+	printf("high: %f\n", sj->high);
+	printf("low: %f\n", sj->low);
+	printf("bid: %f\n", sj->bid);
+	printf("ask: %f\n", sj->ask);
+	printf("volume: %ld\n", sj->volume);
+	printf("amount: %ld\n", sj->amount);
+	for(i = 0; i < 5; i++) {
+		printf("bv%d: %d\n", i, sj->bv[i]);
+		printf("bp%d: %f\n", i, sj->bp[i]);
+		printf("av%d: %d\n", i, sj->av[i]);
+		printf("ap%d: %f\n", i, sj->ap[i]);
+	}
+	printf("date: %s\n", sj->date);
+	printf("time: %s\n", sj->time);
+}
+
+void sinajs_decode(char *buffer, struct sinajs *sj)
+{
+	char *sptr1, *sptr2;
+	char *token;
+	char *p;
+	int i;
+
+	if(!buffer)
+		return;
+
+	token = strtok_r(buffer, "\"", &sptr1);
+	for(p = token; *p && *p != '='; p++)
+		;
+	*p = 0;
+	sscanf(token, "var hq_str_%s", sj->code);
+
+	token = strtok_r(NULL, "\"", &sptr1);
+
+	token = strtok_r(token, ",", &sptr2);
+	strcpy(sj->name, token);
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->open = atof(token);
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->pre_close = atof(token);
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->price = atof(token);
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->high = atof(token);
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->low = atof(token);
+
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->bid = atof(token);
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->ask = atof(token);
+
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->volume = atol(token);
+	token = strtok_r(NULL, ",", &sptr2);
+	sj->amount = atol(token);
+
+	for(i = 0; i < 5; i++) {
+		token = strtok_r(NULL, ",", &sptr2);
+		sj->bv[i] = atoi(token);
+		token = strtok_r(NULL, ",", &sptr2);
+		sj->bp[i] = atof(token);
+	}
+
+	for(i = 0; i < 5; i++) {
+		token = strtok_r(NULL, ",", &sptr2);
+		sj->av[i] = atoi(token);
+		token = strtok_r(NULL, ",", &sptr2);
+		sj->ap[i] = atof(token);
+	}
+
+	token = strtok_r(NULL, ",", &sptr2);
+	strcpy(sj->date, token);
+	token = strtok_r(NULL, ",", &sptr2);
+	strcpy(sj->time, token);
+
+	//psinajs(sj);
+}
 
 int main(int argc, char *argv[])
 {
 	char buffer[4096];
 	char *p;
 	struct sinajs data;
+	char *token;
 
-	http_get("hq.sinajs.cn/list=sh601766,sh601398", buffer, 4096);
-
-	pr_info("result:\n%s", buffer);
-
+	http_get("hq.sinajs.cn/list=sh601766", buffer, 4096);
 	p = split_http_response_header(buffer);
 
-	pr_info("content:\n%s", p);
+	sinajs_decode(p, &data);
 
-	sscanf(p, HTTP_FORMAT_STRING, HTTP_ARGS_LIST(&data));
+	printf("code = %s, price = %.2f\n", data.code, data.price);
 
-	pr_info("code = %s, date = %s\n", data.code, data.date);
-
+	main_ui(argc, argv);
 	return 0;
 }
