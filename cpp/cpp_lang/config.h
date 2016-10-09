77933b07e41801346095b98c0824bb6437c92040
chenqiwei <chenqiwei@szhklt.com>
2016-10-09 14:24:21 +0800

    cpp_lang: hello world

---
+++ cpp/cpp_lang/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "cpplang"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "chqiwei89@163.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "cpplang"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "cpplang 0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "cpplang"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0"
+
+/* Version number of package */
+#define VERSION "0"
